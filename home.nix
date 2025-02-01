919efcfbd4785993587cd265dc113c3621eea4ce
Andrey Nering <andreynering@users.noreply.github.com>
2025-02-01 18:19:57 -0300

    feat: initial working version

--- home.nix
+++ home.nix
@@ -1,76 +1,79 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "andrey";
   home.homeDirectory = "/Users/andrey";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  home.packages = with pkgs; [
+    cargo
+    curl
+    ffmpeg-full
+    git
+    go
+    go-task
+    golangci-lint
+    goreleaser
+    htop
+    jq
+    micro
+    ripgrep
+    rustc
+    rustfmt
+    tree
+    vim
+    wget
+    zsh-completions
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  programs.git = {
+    enable = true;
+    userName = "Andrey Nering";
+    userEmail = "andreynering@users.noreply.github.com";
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    aliases = {
+      br = "branch";
+      ci = "commit";
+      co = "checkout";
+      dc = "diff --cached";
+      st = "status";
+      authors = "!git log --format='%an <%ae>' | sort -u";
+      cleanup = "!git branch -vv | awk '/: gone]/{print $1}' | xargs git br -D";
+      do-gc = "!f() { git reflog expire --all --expire=now && git gc --prune=now --aggressive; }; f";
+      do-merge = "!f() { git merge --no-ff $1 && git branch -d $1; }; f";
+      fixup = "commit --fixup";
+      stash-all = "stash save --include-untracked";
+      undo = "reset --soft HEAD^";
+    };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/andrey/etc/profile.d/hm-session-vars.sh
-  #
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+
+      plugins = [
+        "docker"
+        "git"
+        "kubectl"
+        "node"
+        "npm"
+      ];
+    };
+
+    shellAliases = {
+      t = "task";
+    };
+  };
+
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "micro";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
